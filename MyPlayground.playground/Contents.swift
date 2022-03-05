815422e0ca254003bc9cd48fa096771abf1eb16b
حسين علي العجمي <100701775+yix29@users.noreply.github.com>
2022-03-05 13:59:50 +0300

    ok

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let greeting = "Hello, playground"
+
+let name = "hussain"
+
+var favouriteNumber = 7
+
+var hobby = "footbell"
+ 
+//print("jfmj 1+1\)(name)my hobby is \(hobby)")
+
+print("hi my name is \(name), my hobby is \(hobby) my favouriteNumber \(favouriteNumber)")
+
+var myING = 7
+var myDOUBLE = 7.7
